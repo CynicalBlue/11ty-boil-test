ed3fb5179a44fdc9a3f5bf2025bf220fcf30095a
CynicalBlue <136578998+CynicalBlue@users.noreply.github.com>
2025-11-02 23:04:11 +0000

    Shortcode and layout done

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,11 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("user", function(realName, userName) {
+    return `<div class="user">
+  <div class="real_name">Made by: ${realName}   </div>
+  <div class="user_name">Also known as: ${userName}</div>
+</div>`;
+  });
 };
